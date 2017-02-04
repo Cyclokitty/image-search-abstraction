83b8d17e717cfde33838ba754eb76ee4d1502771
Laura Vingada <laura@vingada.com>
2017-02-04 15:47:15 -0500

    it works!

--- app.js
+++ app.js
@@ -1,30 +1,69 @@
 const express = require('express');
-const MongodbClient = require('mongodb').MongodbClient;
+const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const imageSearch = require('node-google-image-search');
 const dotenv = require('dotenv').config()
 
 const app = express();
 
-app.get('/', (req, res) => {
-  let results = imageSearch('grumpy cat', (results) => {
-    let info = [];
-    for (var i in results) {
-      let url = results[i].link;
-      let snippet = results[i].snippet;
-      let thumbnail = results[i].image.thumbnailLink;
-      let context = results[i].image.contextLink;
-      let infoObj = {url, snippet, thumbnail, context};
-      info.push(infoObj);
-    }
-      console.log(info);
-    res.json(info);
-  }, 0, 5);
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-// change "grumpy cat" to catch the query
-// change 5 to become the offset
+app.get('/api/imagesearch', (req, res) => {
+  MongoClient.connect('mongodb://localhost:27017/imageSearch', (err, db) => {
+    assert.equal(null, err);
+    console.log('Connected to MongoDB server.');
+    db.collection('searched').find().sort({when: -1}).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2));
+        let terms = [];
+        docs.map((doc) => {
+            topic = doc.topic,
+            when = doc.when
+          let search = {topic, when};
+          terms.push(search);
+        });
+        res.json(terms);
+    }, (err) => {
+      console.log('Unable to fetch data', err);
+    });
+  });
+});
 
+app.get('/api/imagesearch/:topic', (req, res) => {
+  let topic = req.params.topic;
+  let offset = req.query.offset || 10;
+  let when = new Date();
+  let results = imageSearch(topic, (results) => {
+    let info = [];
+    results.map((result) => {
+      let url = result.link;
+        let snippet = result.snippet;
+        let thumbnail = result.image.thumbnailLink;
+        let context = result.image.contextLink;
+        let infoObj = {url, snippet, thumbnail, context};
+        info.push(infoObj);
+    });
+    res.json(info);
+  }, 0, offset);
+  console.log(topic, offset, when);
+  MongoClient.connect('mongodb://localhost:27017/imageSearch', (err, db) => {
+    assert.equal(null, err);
+    console.log('Connected to MongoDB server.');
+    db.collection('searched').insertOne({
+      topic: topic,
+      when: when
+    }, (err, result) => {
+      if (err) {
+        res.send('Sorry, we can\'t handle your request right now.');
+        console.log('Error when someone tried making an image search', err);
+      }
+      console.log(result);
+    })
+  })
+});
 
 const port = process.env.PORT || 3000;
 
